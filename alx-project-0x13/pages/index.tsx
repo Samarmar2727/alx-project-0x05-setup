0dd0173abd0c01a694a2b73084853c15f1705d2e
SAMAR KHALED <142172579+Samarmar2727@users.noreply.github.com>
2025-06-21 21:58:25 +0300

    fix: add useFetchData hook and index page

--- alx-project-0x13/pages/index.tsx
+++ alx-project-0x13/pages/index.tsx
@@ -1,4 +1,5 @@
-import ImageCard from "@/components/common/ImageCard";
+// index.tsx
+ import ImageCard from "@/components/common/ImageCard";
 import useFetchData from "@/hooks/useFetchData";
 import { ImageProps } from "@/interfaces";
 import React, { useEffect, useState } from "react";
